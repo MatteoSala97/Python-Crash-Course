25c15f8fb611ddf4fad17a88167ab27cf550f96b
MatteoSala97 <matteo111997@gmail.com>
2024-10-08 10:58:28 +0200

    made a poll

--- 08-10-2024/17-while-loops.py
+++ 08-10-2024/17-while-loops.py
@@ -1,4 +1,4 @@
-def stai_fermo_li_non_runnare():
+def stai_fermo_lì_non_runnare():
     number = 1
     while number <= 20: 
         print(f"{number}")
@@ -20,15 +20,49 @@ def stai_fermo_li_non_runnare():
         else: 
             print(message)
             
-prompt = "\nEnter your toppings (type 'done' when you're done): "
-toppings = []
+    prompt = "\nEnter your toppings (type 'done' when you're done): "
+    toppings = []
 
-while True: 
-   topping = input(prompt).lower()
-   if topping != 'done':
-       toppings.append(topping)
-   else: 
-       break
-   
-print("You have chosen the following toppings:", toppings)
+    while True: 
+        topping = input(prompt).lower()
+        if topping != 'done':
+            toppings.append(topping)
+        else: 
+            break
         
+    print("You have chosen the following toppings:", toppings)
+                
+
+    # Verify each user until there are no more unconfirmed users. 
+    #  Move each verified user into the list of confirmed users.
+    unconfirmed_users = ['alice', 'brian', 'candace']
+    confirmed_users = []
+
+    while unconfirmed_users:
+        current_user = unconfirmed_users.pop()
+        print("Verifying user: " + current_user.title())
+        confirmed_users.append(current_user)
+        
+    # Display all confirmed users.
+    print("\nThe following users have been confirmed:") 
+    for confirmed_user in confirmed_users:
+        print(confirmed_user.title())
+
+# filling a dictionary with user input ( fatto anche nel file precedente ma con una lista)
+responses = {}
+active_polling= True
+
+while active_polling:
+    name = input("\nWhat's your full name?")
+    res = input("\nWould you like to climb someday?")
+    #salva lo user input
+    responses[name] = res
+    
+    repeat = input("Would you like to let another person respond? (yes/ no) ")
+    if repeat == 'no': 
+        active_polling = False
+        
+print("\n--- Poll Results ---")
+print("\nResponses collected:")
+for name, reponse in responses.items():
+    print(f"{name}: {reponse}")
